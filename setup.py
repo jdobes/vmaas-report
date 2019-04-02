182401718bd69c030de03db58917db89baddfb97
Jan Dobes <jdobes@redhat.com>
2019-04-02 11:44:44 +0200

    call vmaas with prepared request JSON file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="vmaas-report",
     description="A tool for getting vulnerabilities from VMaaS for given system package profile",
-    version="0.1.8",
+    version="0.1.9",
     license="MIT",
     author="Jan Dobes",
     author_email="jdobes@redhat.com",
--- vmaas_report/evaluator.py
+++ vmaas_report/evaluator.py
@@ -1,3 +1,5 @@
+import sys
+
 import json
 
 from vmaas_report.rpm_backend import RpmBackend
@@ -8,6 +10,7 @@ class Evaluator:
     def __init__(self):
         self.vmaas_api = VmaasApi()
         self.rpm_backend = RpmBackend()
+        self.errors = 0
 
     def print_status(self):
         print("VMAAS_SERVER: %s" % self.vmaas_api.server)
@@ -21,18 +24,43 @@ class Evaluator:
         print("  Exported: %s" % str(db_change["exported"]))
 
     def print_request(self):
-        print(json.dumps(self._prepare_updates_request(), indent=4, sort_keys=True))
+        request = self._prepare_updates_request()
+        if not request:
+            print("ERROR: VMaaS request creation failed.")
+            self.errors += 1
+            return
+        print(json.dumps(request, indent=4, sort_keys=True))
 
-    def _prepare_updates_request(self):
-        return {
-            "package_list": self.rpm_backend.installed_packages,
-            "repository_list": self.rpm_backend.enabled_repos,
-            "releasever": self.rpm_backend.releasever,
-            "basearch": self.rpm_backend.basearch
-        }
+    def _prepare_updates_request(self, request_file=None):
+        if request_file:
+            if request_file == "-":
+                source = sys.stdin
+            else:
+                source = open(request_file, "r")
+            try:
+                return json.load(source)
+            except json.decoder.JSONDecodeError:
+                print("ERROR: Unable to parse JSON file.")
+                self.errors += 1
+                return None
+            finally:
+                if source != sys.stdin:
+                    source.close()
+        else:
+            return {
+                "package_list": self.rpm_backend.installed_packages,
+                "repository_list": self.rpm_backend.enabled_repos,
+                "releasever": self.rpm_backend.releasever,
+                "basearch": self.rpm_backend.basearch
+            }
 
-    def evaluate_updates(self):
-        response = json.loads(self.vmaas_api.get_updates(self._prepare_updates_request()))
+    def evaluate_updates(self, request_file=None):
+        request = self._prepare_updates_request(request_file=request_file)
+        if not request:
+            print("ERROR: VMaaS request creation failed.")
+            self.errors += 1
+            return
+        response = json.loads(self.vmaas_api.get_updates(request))
         vulnerable_packages = set()
         available_errata = set()
         for old_pkg in response["update_list"]:
